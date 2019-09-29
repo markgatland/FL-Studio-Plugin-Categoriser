50a236dde499346218cd84ba4963ac2417baab5f
markgatland <53534448+markgatland@users.noreply.github.com>
2019-09-29 13:26:47 +0100

    Initial commit

---
+++ Window.Designer.cs
@@ -0,0 +1,165 @@
+﻿using System;
+
+namespace FL_Studio_Plugin_Categoriser
+{
+    partial class Window
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.VSTFolderTextBox = new System.Windows.Forms.TextBox();
+            this.VSTFolderLabel = new System.Windows.Forms.Label();
+            this.VSTFolderButton = new System.Windows.Forms.Button();
+            this.FLFolderButton = new System.Windows.Forms.Button();
+            this.FLFolderLabel = new System.Windows.Forms.Label();
+            this.FLFolderTextBox = new System.Windows.Forms.TextBox();
+            this.CopyAll = new System.Windows.Forms.Button();
+            this.CopyNew = new System.Windows.Forms.Button();
+            this.OutputLog = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // VSTFolderTextBox
+            // 
+            this.VSTFolderTextBox.Location = new System.Drawing.Point(11, 64);
+            this.VSTFolderTextBox.Name = "VSTFolderTextBox";
+            this.VSTFolderTextBox.Size = new System.Drawing.Size(357, 20);
+            this.VSTFolderTextBox.TabIndex = 0;
+            this.VSTFolderTextBox.Text = Properties.Settings.Default.VSTFolder;
+            // 
+            // VSTFolderLabel
+            // 
+            this.VSTFolderLabel.AutoSize = true;
+            this.VSTFolderLabel.Location = new System.Drawing.Point(12, 48);
+            this.VSTFolderLabel.Name = "VSTFolderLabel";
+            this.VSTFolderLabel.Size = new System.Drawing.Size(63, 13);
+            this.VSTFolderLabel.TabIndex = 1;
+            this.VSTFolderLabel.Text = "VST2 folder (from which subfolders will be replicated)";
+            // 
+            // VSTFolderButton
+            // 
+            this.VSTFolderButton.Location = new System.Drawing.Point(374, 64);
+            this.VSTFolderButton.Name = "VSTFolderButton";
+            this.VSTFolderButton.Size = new System.Drawing.Size(75, 23);
+            this.VSTFolderButton.TabIndex = 2;
+            this.VSTFolderButton.Text = "Browse...";
+            this.VSTFolderButton.UseVisualStyleBackColor = true;
+            this.VSTFolderButton.Click += new System.EventHandler(this.VSTFolderButton_Click);
+            // 
+            // FLFolderButton
+            // 
+            this.FLFolderButton.Location = new System.Drawing.Point(374, 25);
+            this.FLFolderButton.Name = "FLFolderButton";
+            this.FLFolderButton.Size = new System.Drawing.Size(75, 23);
+            this.FLFolderButton.TabIndex = 5;
+            this.FLFolderButton.Text = "Browse...";
+            this.FLFolderButton.UseVisualStyleBackColor = true;
+            this.FLFolderButton.Click += new System.EventHandler(this.FLFolderButton_Click);
+            // 
+            // FLFolderLabel
+            // 
+            this.FLFolderLabel.AutoSize = true;
+            this.FLFolderLabel.Location = new System.Drawing.Point(12, 9);
+            this.FLFolderLabel.Name = "FLFolderLabel";
+            this.FLFolderLabel.Size = new System.Drawing.Size(296, 13);
+            this.FLFolderLabel.TabIndex = 4;
+            this.FLFolderLabel.Text = "FL Studio user data folder (usually \'Image-Line\' in Documents)";
+            // 
+            // FLFolderTextBox
+            // 
+            this.FLFolderTextBox.Location = new System.Drawing.Point(11, 25);
+            this.FLFolderTextBox.Name = "FLFolderTextBox";
+            this.FLFolderTextBox.Size = new System.Drawing.Size(357, 20);
+            this.FLFolderTextBox.TabIndex = 3;
+            this.VSTFolderTextBox.Text = Properties.Settings.Default.FLFolder;
+            // 
+            // CopyAll
+            // 
+            this.CopyAll.Location = new System.Drawing.Point(11, 90);
+            this.CopyAll.Name = "CopyAll";
+            this.CopyAll.Size = new System.Drawing.Size(207, 23);
+            this.CopyAll.TabIndex = 6;
+            this.CopyAll.Text = "Copy all plugins\' folder structure";
+            this.CopyAll.UseVisualStyleBackColor = true;
+            this.CopyAll.Click += new System.EventHandler(this.CopyAll_Click);
+            // 
+            // CopyNew
+            // 
+            this.CopyNew.Location = new System.Drawing.Point(225, 91);
+            this.CopyNew.Name = "CopyNew";
+            this.CopyNew.Size = new System.Drawing.Size(207, 23);
+            this.CopyNew.TabIndex = 7;
+            this.CopyNew.Text = "Copy \'New\' plugins\' folder structure";
+            this.CopyNew.UseVisualStyleBackColor = true;
+            this.CopyNew.Click += new System.EventHandler(this.CopyNew_Click);
+            // 
+            // OutputLog
+            // 
+            this.OutputLog.FormattingEnabled = true;
+            this.OutputLog.HorizontalScrollbar = true;
+            this.OutputLog.Location = new System.Drawing.Point(11, 120);
+            this.OutputLog.Name = "OutputLog";
+            this.OutputLog.ScrollAlwaysVisible = true;
+            this.OutputLog.Size = new System.Drawing.Size(438, 212);
+            this.OutputLog.TabIndex = 8;
+            this.OutputLog.Visible = false;
+            // 
+            // Window
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(461, 125);
+            this.Controls.Add(this.OutputLog);
+            this.Controls.Add(this.CopyNew);
+            this.Controls.Add(this.CopyAll);
+            this.Controls.Add(this.FLFolderButton);
+            this.Controls.Add(this.FLFolderLabel);
+            this.Controls.Add(this.FLFolderTextBox);
+            this.Controls.Add(this.VSTFolderButton);
+            this.Controls.Add(this.VSTFolderLabel);
+            this.Controls.Add(this.VSTFolderTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Window";
+            this.Text = "FL Studio Plugin Categoriser";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox VSTFolderTextBox;
+        private System.Windows.Forms.Label VSTFolderLabel;
+        private System.Windows.Forms.Button VSTFolderButton;
+        private System.Windows.Forms.Button FLFolderButton;
+        private System.Windows.Forms.Label FLFolderLabel;
+        private System.Windows.Forms.TextBox FLFolderTextBox;
+        private System.Windows.Forms.Button CopyAll;
+        private System.Windows.Forms.Button CopyNew;
+        private System.Windows.Forms.ListBox OutputLog;
+    }
+}
+
---
+++ Window.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace FL_Studio_Plugin_Categoriser
+{
+    public partial class Window : Form
+    {
+        public Window()
+        {
+            InitializeComponent();
+            if (Properties.Settings.Default.FLFolder == "")
+            {
+                this.FLFolderTextBox.Text = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\Documents\\Image-Line";
+                Properties.Settings.Default.FLFolder = this.FLFolderTextBox.Text;
+                Properties.Settings.Default.Save();
+            }
+        }
+        private void VSTFolderButton_Click(object sender, EventArgs e)
+        {
+            using (var dialog = new System.Windows.Forms.FolderBrowserDialog())
+            {
+                System.Windows.Forms.DialogResult result = dialog.ShowDialog();
+                VSTFolderTextBox.Text = dialog.SelectedPath;
+                Properties.Settings.Default.VSTFolder = dialog.SelectedPath;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        private void FLFolderButton_Click(object sender, EventArgs e)
+        {
+            using (var dialog = new System.Windows.Forms.FolderBrowserDialog())
+            {
+                System.Windows.Forms.DialogResult result = dialog.ShowDialog();
+                FLFolderTextBox.Text = dialog.SelectedPath;
+                Properties.Settings.Default.FLFolder = dialog.SelectedPath;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        private void CopyAll_Click(object sender, EventArgs e)
+        {
+            CopyPluginStructure(new String[4] { "Installed\\Effects\\VST\\", "Installed\\Generators\\VST\\", "Installed\\Effects\\New\\", "Installed\\Generators\\New\\" });
+        }
+
+        private void CopyNew_Click(object sender, EventArgs e)
+        {
+            CopyPluginStructure(new String[2] { "Installed\\Effects\\New\\", "Installed\\Generators\\New\\" });
+        }
+
+        private void CopyPluginStructure(string[] folders)
+        {
+            OutputLog.Show();
+            ClientSize = new System.Drawing.Size(461, 343);
+            int missing = 0;
+            String database;
+            if (FLFolderTextBox.Text[FLFolderTextBox.Text.Length - 1].ToString() != "\\")
+            {
+                database = FLFolderTextBox.Text + "\\FL Studio\\Presets\\Plugin database\\";
+            }
+            else
+            {
+                database = FLFolderTextBox.Text + "FL Studio\\Presets\\Plugin database\\";
+            }
+            foreach (string folder in folders)
+            {
+                String directory = database + folder;
+                if (!Directory.Exists(directory))
+                {
+                    missing++;
+                    continue;
+                }
+                foreach (string filename in Directory.GetFiles(directory))
+                {
+                    if (filename.Contains(".nfo"))
+                    {
+                        if (File.Exists(filename.Substring(0, filename.Length - 4) + ".fst"))
+                        {
+                            String text;
+                            try
+                            {
+                                StreamReader sr = new StreamReader(filename);
+                                text = sr.ReadToEnd();
+                                Regex location = new Regex(@"ps_file_filename_0=.*?\.dll"); // Todo: Simplify this - don't need regex anymore
+                                Match match = location.Match(text);
+                                if (match.Success)
+                                {
+                                    if (match.Value.Contains(VSTFolderTextBox.Text))
+                                    {
+                                        text = match.Value;
+                                        text = text.Replace("ps_file_filename_0=" + VSTFolderTextBox.Text, ""); // Todo: Worry about double slash
+                                        for (int i = 1; i < text.Length; i++)
+                                        {
+                                            if (text[text.Length-i].ToString() == "\\")
+                                            {
+                                                text = text.Substring(0, text.Length - i);
+                                                break;
+                                            }
+                                        }
+                                        if (!Directory.Exists(database + text))
+                                        {
+                                            Directory.CreateDirectory(database + text);
+                                            
+                                        }
+                                        File.Copy(filename.Substring(0, filename.Length - 4) + ".fst", database + text + "\\" + Path.GetFileName(filename).Substring(0, Path.GetFileName(filename).Length - 4) + ".fst", true);
+                                        OutputLog.Items.Add("Copied folder structure for '" + filename.Substring(0, filename.Length - 4) + "'");
+                                    }
+                                    else
+                                    {
+                                        OutputLog.Items.Add("Error copying '" + filename.Substring(directory.Length, filename.Length - directory.Length - 4) + "': VST file located outside of specified folder. (Try re-scanning plugins in FL Studio)");
+                                        // Todo: Be more concise with this error as it's an issue with the FL plugin database, not the actual VST file
+                                    }
+                                }
+                                else
+                                {
+                                    OutputLog.Items.Add("Error copying '" + filename.Substring(directory.Length, filename.Length - directory.Length - 4) + "': No associated VST file could be found.");
+                                }
+                                sr.Close();
+                                Console.ReadLine();
+                            }
+                            catch (Exception e)
+                            {
+                                OutputLog.Items.Add("Error copying '" + filename.Substring(directory.Length, filename.Length - directory.Length) + "': Unhandled exception");
+                                OutputLog.Items.Add(e); // Todo: Maybe move this try catch to just the file open, place rest of code after (*if* open file exists, match contents etc) 
+                            }
+                        }
+                        else
+                        {
+                            OutputLog.Items.Add("Couldn't find .fst for " + filename.Substring(directory.Length, filename.Length - directory.Length) + " (It's probably part of another VST)");
+                        }
+                    }
+                }
+            }
+            if (missing == 4)
+            {
+                OutputLog.Items.Add("Couldn't find any plugins. Try running a plugin scan in FL Studio.");
+            }
+        }
+    }
+}
